5c0f0a2ae70885e0c98738ecc4aa98d6656f929c
Jordan Gunning <42986184+Xeleian@users.noreply.github.com>
2018-09-29 10:57:42 -0400

    Project File: Just Testing

---
+++ Just Testing/Assets/Camera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    private GameObject player;
+    public float xLeftBound;
+    public float xRightBound;
+    public float yUpBound;
+    public float yDownBound;
+
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        float x = Mathf.Clamp(player.transform.position.x, xLeftBound, xRightBound);
+        float y = Mathf.Clamp(player.transform.position.y, yUpBound, yDownBound);
+        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+    }
+}
---
+++ Just Testing/Assets/Player_Movement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+
+    public int playerMovementSpeed = 10; //Change?
+    public int playerJumpHeight = 2000; //Change?
+    public bool playerDirectionR = true;
+    public float MoveXAxis;
+    public float MoveYAxis;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerMove();
+	}
+
+	//Holds movement controls, animations, character physics
+	void PlayerMove() {
+        MoveXAxis = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown ("Jump"))
+        {
+            Jumping();
+        }
+        if(MoveXAxis > 0.0f && playerDirectionR == false)
+        {
+            FlipSprite();
+        }
+        if (MoveXAxis < 0.0f && playerDirectionR == true)
+        {
+            FlipSprite();
+        }
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(MoveXAxis * playerMovementSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    //Allows the player to jump
+    void Jumping()
+    {
+        int jumpCounter = 1;
+        print("Testing");
+        if (jumpCounter != 0)
+        {
+            MoveYAxis = Input.GetAxis("Vertical");
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpHeight);
+            
+
+        }
+
+        
+    }
+    //Flips player sprite when changing directions, left and right.
+	void FlipSprite() {
+        playerDirectionR = !playerDirectionR;
+        Vector2 direction = gameObject.transform.localScale;
+        direction.x *= -1;
+        transform.localScale = direction;
+	}
+
+	}
