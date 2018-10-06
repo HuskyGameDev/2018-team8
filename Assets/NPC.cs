9fa8316142feb2b587bc240c710af964973b7c78
Tristan Sorensen <tasorens@mtu.edu>
2018-10-06 19:05:45 -0400

    Everything Fixed?

--- Assets/NPC.cs
+++ Assets/NPC.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/* using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,3 +23,5 @@ public class NPC : Monobehaviour {
     }
 
 }
+
+*/
--- Assets/Wander.cs
+++ Assets/Wander.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/* using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -33,3 +33,4 @@ public class Wander : MonoBehaviour {
 
 
 }
+*/
