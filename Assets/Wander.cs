6331076e825ee4eb7f087be97cfea128f6abfa20
Kyle Alberth <kealbert@mtu.edu>
2018-10-06 18:04:12 -0400

    Wander Script

---
+++ Assets/Wander.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour {
+
+    Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.autoBraking = false;
+
+        GoToNextPoint();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!agent.PathPending && agent.remainingDistance < 0.5f)
+            GoToNextPoint();
+	}
+
+    GoToNextPoint() {
+        if (points.Length == 0)
+            return;
+
+        agent.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+}
