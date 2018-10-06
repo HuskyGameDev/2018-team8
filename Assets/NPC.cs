bab139164245c21fe996d22e7daeaeeb52ce6d32
Kyle Alberth <kealbert@mtu.edu>
2018-10-06 17:27:28 -0400

    NPC base

---
+++ Assets/NPC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : Monobehaviour {
+
+    //NPC stats subject to change
+    int attackDamage = 1;
+    int health = 5;
+    float npcMoveSpeed = 10f;
+
+    GameObject player;
+    CharacterController characterController;
+    float moveXAxis;
+
+	void Start () {
+        characterController = GetComponent<characterController>();
+    }
+
+    
+    void Update() {
+        transform.poisition += new Vector2(npcMoveSpeed * time.deltaTime, 0, 0);
+    }
+
+}
