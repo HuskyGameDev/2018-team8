2d53367bf7167ca774aa1a914616c61c1d4aa5f1
Jordan Gunning <42986184+Xeleian@users.noreply.github.com>
2018-10-19 18:54:00 -0400

    PlayerDeath

---
+++ Assets/PlayerDeath.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDeath : MonoBehaviour
+{
+
+    public int HP;
+    public bool died;
+
+    // Use this for initialization
+    void Start()
+    {
+        died = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y < -6)
+        {
+            died = true;
+        }
+        if (died == true)
+        {
+            StartCoroutine("Death");
+        }
+    }
+    void Death()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
+
