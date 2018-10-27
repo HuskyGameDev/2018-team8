c91b356db9a4319e8084f2b1ac20788a1416b287
Ethan Manarin <kindanon@users.noreply.github.com>
2018-10-27 17:49:45 -0400

    added falling platform

---
+++ Assets/fallingPlat.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingPlat : MonoBehaviour
+{
+
+    private Rigidbody2D plat;
+    public float fallDelay = 2.0f;
+
+    private void Start()
+    {
+        plat = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Player"))
+        {
+            StartCoroutine(fall());
+        }
+    }
+
+    IEnumerator fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        plat.isKinematic = false;
+        GetComponent<Collider2D>().isTrigger = true;
+        
+    }
+}
