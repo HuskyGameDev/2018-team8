27af1f1ff6329cdc5c26b4a0949039dff488e225
Ethan Manarin <kindanon@users.noreply.github.com>
2018-12-02 18:18:38 -0500

    score + coins
    added coins that increment score counter + ui

---
+++ Assets/coins.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class coins : MonoBehaviour {
+
+    //public Text score;
+    //public int points = 0;
+    public int value = 1;
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            StartCoroutine("coin");
+        }
+
+    }
+
+    void coin()
+    {
+        //std_speed = Player_Movement.instace.playerMovementSpeed;
+        coinmanager.instance.points += value;
+        Destroy(gameObject);
+    }
+}
