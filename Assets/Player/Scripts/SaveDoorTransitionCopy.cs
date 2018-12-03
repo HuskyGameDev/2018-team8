fe3534f20fdda027025a77823b5639a756b50eac
Jordan Gunning <42986184+Xeleian@users.noreply.github.com>
2018-12-02 21:34:05 -0500

    Door and Transition

---
+++ Assets/Player/Scripts/SaveDoorTransitionCopy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SaveDoorTransitionCopy : MonoBehaviour
+{
+    [SerializeField] private string level;  //Set what level the door will take you//
+    [SerializeField] private string thing;  //Set what triggers this to occur, usually our player//
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Entered");
+        if (collision.gameObject.name.Equals(thing) && Input.GetButtonDown("Interact"))
+        {
+            SceneManager.LoadScene(level);
+        }
+    }
+}
