7848a5edb301aa2c3b74a8f0c4cc7b2d93a51b03
Tristan Sorensen <tasorens@mtu.edu>
2018-12-02 19:07:52 -0500

    Added Log, Falling Spike, Turret Scripts
    Read Summary ^

---
+++ Assets/Platfoms/Hazards/log.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class log : MonoBehaviour {
+    Rigidbody2D rb;
+    public int rollSpeed = -3;
+    bool hit;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.gameObject.name.Equals("CharacterPrototype (1)")) {
+            this.rb.isKinematic = false;
+            hit = true;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (hit)
+        {
+            rb.velocity = new Vector2(rollSpeed, rb.velocity.y);
+        }
+	}
+}
