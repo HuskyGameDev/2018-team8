2471dc87c4cf2b94a97c2fb92d107f3b9ad04640
Ethan Manarin <kindanon@users.noreply.github.com>
2018-12-09 20:53:48 -0500

    added cam zoom
    camzoom
    camzoomout
    
    add to collider to use

---
+++ Assets/camzoomout.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camzoomout : MonoBehaviour
+{
+
+    public Camera cam;
+    int numb = 100;
+    int iter;
+    public int defaultsize = 5;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            iter = 120;
+            //StartCoroutine("zoom");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (iter > 0 && cam.orthographicSize <= defaultsize)
+        {
+            cam.orthographicSize -= (float).1;
+        }
+        iter--;
+
+    }
+}
