7e652fb58ce07b75479cdbc38c5b752c15840b99
Tristan Sorensen <tasorens@mtu.edu>
2019-04-07 15:55:05 -0400

    EnemyHealth, Fixed PacMan Prefab, Tagged Bullets
    Enemies can now die from being damaged, PacMan prefab now properly up to date, bullets now have tags for helping with damage

---
+++ Assets/Hazards/Enemy/enemyHealth.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyHealth : MonoBehaviour
+{
+
+    public int health = 3;
+    bool hit;
+    int damageTimer = 0;
+    public int invulnTime = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        damageTimer = 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Damage") || collision.collider.CompareTag("Bullet")) {
+            if (!hit)
+            {
+                health--;
+                if(health == 0)
+                {
+                    death();
+                }
+                hit = true;
+            }
+        }
+
+        if (collision.collider.CompareTag("Killer"))
+        {
+            death();
+        }
+    }
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if (hit)
+        {
+            damageTimer++;
+        }
+
+        if (damageTimer == invulnTime)
+        {
+            hit = false;
+            damageTimer = 0;
+        }
+    }
+
+    void death()
+    {
+        Destroy(gameObject);
+    }
+}
