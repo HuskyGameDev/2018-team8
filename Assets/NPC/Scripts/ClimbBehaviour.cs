817b7f23915aaca3f741e36131c6fa92e6ace4b0
Kyle Alberth <kealbert@mtu.edu>
2019-04-13 18:53:17 -0400

    Boss Fight
    Mostly working, needs animations

---
+++ Assets/NPC/Scripts/ClimbBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbBehaviour : StateMachineBehaviour
+{
+    private float speed = 3;
+    public GameObject top, bottom;
+    private GameObject next;
+
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        top = GameObject.FindGameObjectWithTag("BossTop");
+        bottom = GameObject.FindGameObjectWithTag("BossBottom");
+        if ( animator.transform.position == top.transform.position )
+        {
+            next = bottom;
+        }
+        else
+        {
+            next = top;
+        }
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.transform.position = Vector2.MoveTowards(animator.transform.position, next.transform.position, speed * Time.deltaTime);
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
---
+++ Assets/NPC/Scripts/SpiderBoss.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpiderBoss : MonoBehaviour
+{
+
+    public int maxHp = 50;
+    public int currentHp = 50;
+    public int damage = 1;
+    private float damageWait = 1.5f;
+
+    public Slider healthBar;
+    public Transform Bottom, Top;
+
+    bool isDead = false;
+    bool twoThirds = false;
+    bool oneThird = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (damageWait > 0)
+        {
+            damageWait -= Time.deltaTime;
+        }
+        healthBar.value = currentHp;
+
+        if (currentHp == 0)
+        {
+            isDead = true;
+        }
+
+        if (currentHp <= 2/3 * maxHp)
+        {
+            twoThirds = true;
+        }
+        
+        if ( currentHp <= 1/3 * maxHp)
+        {
+            oneThird = true;
+        }
+    }
+}
