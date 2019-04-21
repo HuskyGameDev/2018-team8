13ba3c19a36b8bbfee69add06d2e7a49b0985b18
Ethan Manarin <kindanon@users.noreply.github.com>
2019-04-20 20:53:42 -0400

    pmenu script
    forgot to upload script for pause menu

---
+++ Assets/GUI/pmenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pmenu : MonoBehaviour {
+
+    public static bool paused = false;
+    public GameObject menu;
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (paused)
+                resume();
+            else
+            {
+                pause();
+            }
+        }
+	}
+
+    void resume()
+    {
+        menu.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+    }
+
+    void pause()
+    {
+        menu.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+
+    }
+}
+
